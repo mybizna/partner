6ab0ad584c791f04bc989c10b5f89bfe19780370
Dedan Irungu <dedanirungu@gmail.com>
2022-09-15 14:46:27 +0300

    Update

--- menu.php
+++ menu.php
@@ -4,12 +4,9 @@ $this->add_module_info("partner", [
     'title' => 'Partner',
     'description' => 'Partner',
     'icon' => 'fas fa-store',
-    'path' => '/products',
+    'path' => 'partner.admin.partner',
     'class_str'=> 'text-info border-info'
 ]);
 
 
-$this->add_menu("partner", "dashboard", "Dashboard", "/dashboard", "fas fa-cogs", 1);
-$this->add_menu("partner", "customers", "Customers", "/users/customers", "fas fa-cogs", 5);
-$this->add_menu("partner", "vendors", "Vendors", "/users/vendors", "fas fa-cogs", 5);
-$this->add_menu("partner", "employees", "Employees", "/users/employees", "fas fa-cogs", 5);
+$this->add_menu("partner", "partner", "Partners", "/partner/admin/partner", "fas fa-cogs", 5);
