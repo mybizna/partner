9a09319ba6475c8d01dd16c29893fdca40b714fa
Dedan Irungu <dedanirungu@gmail.com>
2023-01-26 16:45:14 +0300

    Working on invoice auto reconcilation and auto partner discovery

--- Classes/Partner.php
+++ Classes/Partner.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Partner\Classes;
 
+use App\Models\User;
 use Modules\Partner\Entities\Partner as DBPartner;
 use Modules\Partner\Entities\Slug;
 
@@ -39,22 +40,101 @@ class Partner
                 $partner = DBPartner::create($data);
             }
 
-            
             $slugs[] = $partner->id;
-            
+
             foreach ($slugs as $key => $slug) {
-                $slug_where = [['partner_id', $partner->id], ['slug', $slug]];
-                $partner_slugs = Slug::where($slug_where)->first();
-                
-                if (!$partner_slugs) {
-                    Slug::create(['partner_id' => $partner->id, 'slug' => $slug]);
-                }
+                $this->updatePartnerSlug($partner->id, $slug);
             }
-            
+
             return $partner;
         }
 
         return false;
+
+    }
+
+    public function getPartner($partner_str)
+    {
+        $partner_slug = Slug::where('slug', $partner_str)->first();
+
+        if ($partner_slug) {
+            $partner = DBPartner::where('id', $partner_slug->partner_id)->first();
+            if ($partner) {
+                return $partner;
+            }
+        }
+
+        $partner = DBPartner::where('email', $partner_str)
+            ->orWhere('phone', $partner_str)
+            ->orWhere('mobile', $partner_str)
+            ->first();
+
+        if ($partner) {
+
+            $this->updatePartnerSlug($partner->id, $partner->email);
+            $this->updatePartnerSlug($partner->id, $partner->phone);
+            $this->updatePartnerSlug($partner->id, $partner->mobile);
+
+            return $partner;
+        } else {
+
+            $user = User::where('username', $partner_str)
+                ->orWhere('phone', $partner_str)
+                ->orWhere('email', $partner_str)
+                ->first();
+
+            if ($user) {
+                $partner = DBPartner::where('email', $user->email)
+                    ->orWhere('phone', $user->phone)
+                    ->orWhere('mobile', $user->phone)
+                    ->first();
+
+                if ($partner) {
+
+                    $this->updatePartnerSlug($partner->id, $partner->email);
+                    $this->updatePartnerSlug($partner->id, $partner->phone);
+                    $this->updatePartnerSlug($partner->id, $partner->mobile);
+
+                    return $partner;
+
+                } else {
+
+                    $name = explode(' ', $user->name);
+                    $data = [
+                        'first_name' => $name[0],
+                        'last_name' => $name[1],
+                        'email' => $user->email,
+                        'phone' => $user->phone,
+                        'slug' => [$user->username, $user->email, $user->phone],
+                    ];
+
+                    $partner = $this->createPartner($data);
+
+                    if ($partner) {
+                        return $partner;
+                    }
+                }
+            }
+
+        }
+
+        return false;
+
+    }
+
+    public function updatePartnerSlug($partner_id, $slug)
+    {
+
+        if ($slug == '') {
+            return;
+        }
+
+        $slug_where = [['partner_id', $partner_id], ['slug', $slug]];
+        $partner_slugs = Slug::where($slug_where)->first();
+
+        if (!$partner_slugs) {
+            Slug::create(['partner_id' => $partner->id, 'slug' => $slug]);
+        }
 
     }
 }
