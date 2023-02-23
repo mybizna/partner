1e7ac75b4146b7a7ffb29702b50566c3b135db4e
Dedan Irungu <dedanirungu@gmail.com>
2023-02-23 16:04:57 +0300

    Improvement to table listing

--- views/admin/partner/list.vue
+++ views/admin/partner/list.vue
@@ -1,38 +1,34 @@
 <template>
-    <table-render title="Partner" :path_param="path_param" :search_fields="search_fields" :model="model"
-        :table_fields="table_fields"></table-renderer>
+    <table-render title="Partner" :path_param="['partner', 'partner']" :table_fields="table_fields">
+
+        <template #header>
+            <th-render>First Name</th-render>
+            <th-render>Last Name</th-render>
+            <th-render>Email</th-render>
+            <th-render>Phone</th-render>
+            <th-render>Mobile</th-render>
+            <th-render>Country</th-render>
+            <th-render>Currency</th-render>
+        </template>
+
+        <template #body="{ item }">
+            <td>{{ item.first_name }}</td>
+            <td>{{ item.last_name }}</td>
+            <td>{{ item.email }}</td>
+            <td>{{ item.phone }}</td>
+            <td>{{ item.mobile }}</td>
+            <td>{{ item.country_id }}</td>
+            <td>{{ item.currency_id }}</td>
+        </template>
+
+    </table-render>
 </template>
 
 <script>
 export default {
-    data () {
+    data() {
         return {
-            path_param: ["partner", "partner"],
-            model: {
-                user_id: "",
-                first_name: "",
-                last_name: "",
-                email: "",
-                phone: "",
-                country_id: "",
-            },
-
-            search_fields: [
-                { type: "text", name: "first_name", label: "First Name", ope: "", },
-                { type: "text", name: "last_name", label: "Last Name", ope: "", },
-                { type: "text", name: "email", label: "Email", ope: "", },
-                { type: "text", name: "mobile", label: "Mobile", ope: "", },
-                { type: "text", name: "country_id", label: "Country", ope: "", },
-            ],
-            table_fields: [
-                { text: "First Name", prop: "first_name", name: "first_name", },
-                { text: "Last Name", prop: "last_name", name: "last_name", },
-                { text: "Email", prop: "email", name: "email", },
-                { text: "Phone", prop: "phone", name: "phone", },
-                { text: "Mobile", prop: "mobile", name: "mobile", },
-                { text: "Country", prop: "country_id", name: "country_id", },
-                { text: "Currency", prop: "currency_id", name: "currency_id", },
-            ],
+            table_fields: ['first_name', 'last_name', 'email', 'phone', 'mobile', 'country_id', 'currency_id'],
         };
     },
 };
