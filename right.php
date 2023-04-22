951eb2b440c841cbcb20a2172884e89908265dd0
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:14 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,43 @@
+<?php
+
+$this->add_right("partner", "life_stage", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "life_stage", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "life_stage", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "life_stage", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "life_stage", "registered", view:true, add:true);
+$this->add_right("partner", "life_stage", "guest", view:true, );
+
+$this->add_right("partner", "meta", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "meta", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "meta", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "meta", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "meta", "registered", view:true, add:true);
+$this->add_right("partner", "meta", "guest", view:true, );
+
+$this->add_right("partner", "partner", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "partner", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "partner", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "partner", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "partner", "registered", view:true, add:true);
+$this->add_right("partner", "partner", "guest", view:true, );
+
+$this->add_right("partner", "slug", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "slug", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "slug", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "slug", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "slug", "registered", view:true, add:true);
+$this->add_right("partner", "slug", "guest", view:true, );
+
+$this->add_right("partner", "type", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "type", "registered", view:true, add:true);
+$this->add_right("partner", "type", "guest", view:true, );
+
+$this->add_right("partner", "type_relation", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type_relation", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type_relation", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("partner", "type_relation", "staff", view:true, add:true, edit:true);
+$this->add_right("partner", "type_relation", "registered", view:true, add:true);
+$this->add_right("partner", "type_relation", "guest", view:true, );
