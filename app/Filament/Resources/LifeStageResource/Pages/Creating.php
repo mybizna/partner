f5b7e8c128c47ca8d3a425659f245ad1024d4e06
Dedan <dedanirungu@gmail.com>
2024-10-13 16:35:12 +0300

    Release 2024.13

---
+++ app/Filament/Resources/LifeStageResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Partner\Filament\Resources\LifeStageResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
---
+++ app/Filament/Resources/TypeRelationResource/Pages/Listing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Partner\Filament\Resources\TypeRelationResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\ListingBase;
+
+// Class List that extends ListBase
+class Listing extends ListingBase
+{
+}
